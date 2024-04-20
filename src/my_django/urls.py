5ff827aa2a3099f8d3e9676cc77b04df1e458160
silvhua <silvhua@gmail.com>
2024-04-20 10:01:15 -0700

    copied scripts from tutorial to see if it would work; no luck; tutorial scripts stopped working

--- src/my_django/urls.py
+++ src/my_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for scripts project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("semantic_search/", include("semantic_search.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
--- src/semantic_search/urls.py
+++ src/semantic_search/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.retrieve, name="retrieve"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
 ]
