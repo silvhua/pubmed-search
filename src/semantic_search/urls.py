585c65a7bbb2531a9cca6eea27af8a9eaf95e3a3
silvhua <silvhua@gmail.com>
2024-04-20 18:04:09 -0700

    allow for query to be passed in the url

--- src/semantic_search/urls.py
+++ src/semantic_search/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("retrieve", views.retrieve, name="retrieve"),
+    path("retrieve/<str:query>/", views.retrieve, name="retrieve"),
     path("index", views.index, name="index")
 ]
