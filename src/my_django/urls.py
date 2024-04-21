a283c52dfd8ccc9fab4735830e59c9d4ef4d6278
silvhua <silvhua@gmail.com>
2024-04-20 17:06:03 -0700

    able to see query results at `http://127.0.0.1:8080/retrieve`

--- src/my_django/urls.py
+++ src/my_django/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("semantic_search/", include("semantic_search.urls")),
+    path("", include("semantic_search.urls")),
     path("admin/", admin.site.urls),
 ]
--- src/semantic_search/urls.py
+++ src/semantic_search/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("retrieve", views.retrieve, name="retrieve"),
+    path("index", views.index, name="index")
 ]
